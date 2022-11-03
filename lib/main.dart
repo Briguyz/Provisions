f5f211e4f9c9371315dc3959e058ab26203cd076
thao0895 <thao0895@d.umn.edu>
2022-11-03 16:37:35 -0500

    thao0895 test commit ?

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Provisions',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: MyColors.provisionGreen,
       ),
-      home: const MyHomePage(title: 'Provision food banks'),
+      home: const MyHomePage(title: 'Provisions'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -79,20 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -113,3 +81,22 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+class MyColors{
+  static const MaterialColor provisionGreen = const MaterialColor(
+    0xFF3DE172,
+    <int, Color>{
+      50: Color(0xFF3DE172),
+      100: Color(0xFF3DE172),
+      200: Color(0xFF3DE172),
+      300: Color(0xFF3DE172),
+      400: Color(0xFF3DE172),
+      500: Color(0xFF3DE172),
+      600: Color(0xFF3DE172),
+      700: Color(0xFF3DE172),
+      800: Color(0xFF3DE172),
+      900: Color(0xFF3DE172),
+    },
+  );
+}
+
+
